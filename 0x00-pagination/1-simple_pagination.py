b14dc502088566ea61b8d45d33840068b0a20a09
Alshimaa Mamdouh <48684686+alshimaamamdouh@users.noreply.github.com>
2024-06-25 02:52:31 +0300

    Update 1-simple_pagination.py

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -34,11 +34,12 @@ class Server:
         return (start, end)
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """get_page
+        """Retrieves a page of data.
         """
         assert type(page) == int and type(page_size) == int
         assert page > 0 and page_size > 0
         start, end = index_range(page, page_size)
-        if start > len(dataset(self)):
+        data = self.dataset()
+        if start > len(data):
             return []
-        return (dataset(self))[start:end]
+        return data[start:end]
