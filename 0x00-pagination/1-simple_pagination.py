b64c46df69d41885983950534e6446d4662ae507
Alshimaa Mamdouh <48684686+alshimaamamdouh@users.noreply.github.com>
2024-06-25 02:53:44 +0300

    Update 1-simple_pagination.py

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -39,7 +39,7 @@ class Server:
         assert type(page) == int and type(page_size) == int
         assert page > 0 and page_size > 0
         start, end = index_range(page, page_size)
-        data = self.dataset()
+        data = dataset(self)
         if start > len(data):
             return []
         return data[start:end]
