fef6d32d39132d5c48d3f58c0644e76bee833c18
alshimaamamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2024-06-27 00:20:12 +0000

    task0

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -34,4 +34,4 @@ class FIFOCache(BaseCaching):
     def get(self, key):
         '''return the value in `self.cache_data` linked to `key`
         '''
-        return self.cache_data.get(key)
+        return self.cache_data.get(key, None)
